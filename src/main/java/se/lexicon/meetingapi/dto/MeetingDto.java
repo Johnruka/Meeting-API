725ae51bd7fd75d5c55438eda18efc8c4a0dca3d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-04 18:37:32 +0100

    Created and added implemented

---
+++ src/main/java/se/lexicon/meetingapi/dto/MeetingDto.java
@@ -0,0 +1,37 @@
+package se.lexicon.meetingapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record MeetingDto(
+
+        Long id, @NotBlank(message = "Title is required")
+        @Size(max = 100, message = "Title must not exceed 100 characters")
+        String title,
+
+        @NotBlank(message = "Date is required")
+        @Pattern(
+                regexp = "\\d{4}-\\d{2}-\\d{2}",
+                message = "Date must be in the format YYYY-MM-DD"
+        )
+        String date,
+
+        @NotBlank(message = "Time is required")
+        @Pattern(
+                regexp = "(?:[01]\\d|2[0-3]):[0-5]\\d (?:AM|PM)",
+                message = "Time must be in the format HH:MM AM/PM"
+        )
+        String time,
+
+        @NotBlank(message = "Level is required")
+        @Size(max = 255, message = "Location must not exceed 255 characters")
+        String level,
+
+        @NotBlank(message = "Action is required")
+        @Pattern(
+                regexp = "delete"
+        )
+        String actions
+) {
+}
---
+++ src/main/java/se/lexicon/meetingapi/repository/MeetingRepository.java
@@ -0,0 +1,7 @@
+package se.lexicon.meetingapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.lexicon.meetingapi.entity.Meeting;
+
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+}
