040a2354efd5264902d4ee57c55c0143c396057d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-09 21:37:52 +0100

    modified

--- src/main/java/se/lexicon/meetingapi/entity/Meeting.java
+++ src/main/java/se/lexicon/meetingapi/entity/Meeting.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Meeting {
@@ -24,16 +26,6 @@ public class Meeting {
     private String status;
 
     public Meeting(String title, String date, String time, String level, String location, String status) {
-        this.title = title;
-        this.date = date;
-        this.time = time;
-        this.level = level;
-        this.location = location;
-        this.status = status;
-    }
-
-    public Meeting(Long id, String title, String date, String time, String level, String location, String status) {
-        this.id = id;
         this.title = title;
         this.date = date;
         this.time = time;
