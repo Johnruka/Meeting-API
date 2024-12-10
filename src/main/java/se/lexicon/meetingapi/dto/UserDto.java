bc4df90e73d22f065e4f6a247939b2beb3df943b
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-10 20:22:01 +0100

    created and added methods

---
+++ src/main/java/se/lexicon/meetingapi/dto/UserDto.java
@@ -0,0 +1,26 @@
+package se.lexicon.meetingapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserDto(
+
+Long id, @NotBlank(message = "User is required")
+
+@NotBlank(message = "firstName is required")
+@Size(max = 50, message = "firstName must not exceed 50 characters")
+String firstName,
+
+@NotBlank(message = "lastName is required")
+@Size(max = 50, message = "lastName must not exceed 50 characters")
+String lastName,
+
+@NotBlank(message = "email is required")
+@Size(max = 50, message = "email must not exceed 50 characters")
+String email,
+
+@NotBlank(message = "role is required")
+@Size(max = 50, message = "role must not exceed 50 characters")
+String role
+        ) {
+}
---
+++ src/main/java/se/lexicon/meetingapi/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package se.lexicon.meetingapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.lexicon.meetingapi.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
