a5704e8f786bf2712ddbc5a50696ebcf457e99be
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-15 17:32:32 +0100

    modified

--- src/main/java/se/lexicon/meetingapi/config/MeetingDataBaseInitializer.java
+++ src/main/java/se/lexicon/meetingapi/config/MeetingDataBaseInitializer.java
@@ -2,6 +2,7 @@ package se.lexicon.meetingapi.config;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+import se.lexicon.meetingapi.entity.Invitation;
 import se.lexicon.meetingapi.entity.Meeting;
 import se.lexicon.meetingapi.repository.MeetingRepository;
 
@@ -23,6 +24,7 @@ public class MeetingDataBaseInitializer {
         List<Meeting> meetings = Arrays.asList(
                 new Meeting("Morning Meeting", "2024-12-25", "10:00 AM", "12:30 PM" , "Zoom", "Team"),
                 new Meeting("Lecture", "2024-12-26", "2:00 PM", "4:00 PM", "Class room","department"),
+                new Meeting("Class Meeting", "2024-12-28", "1:00 PM", "4:00 PM", "Main hall", "company"),
                 new Meeting("Weekly Meeting", "2024-12-28", "1:00 PM", "3:00 PM", "Main Office", "company"),
                 new Meeting("Evening Meeting", "2024-12-30", "11:00 AM", "11:45 AM", "Meeting room","Team"),
                 new Meeting("Monthly Meeting", "2024-12-16", "10:00 AM", "11:30 AM", "Main hall", "department")
