c6e11433e53f4bf45db687df37786fec08378039
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-06 17:54:50 +0100

    modified

--- src/main/java/se/lexicon/meetingapi/dto/MeetingDto.java
+++ src/main/java/se/lexicon/meetingapi/dto/MeetingDto.java
@@ -2,48 +2,35 @@ package se.lexicon.meetingapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record MeetingDto(
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MeetingDto {
 
-        Long id, @NotBlank(message = "Meeting is required")
-        @Size(max = 100, message = "Title must not exceed 100 characters")
-        String title,
+        private Long id;
+
+        @NotBlank(message = "Title is required")
+        private String title;
 
         @NotBlank(message = "Date is required")
-        @Pattern(
-                regexp = "\\d{4}-\\d{2}-\\d{2}",
-                message = "Date must be in the format YYYY-MM-DD"
-        )
-        String date,
+        private String date;
 
-        @NotBlank(message = "Time is required")
-        @Pattern(
-                regexp = "(?:[01]\\d|2[0-3]):[0-5]\\d (?:AM|PM)",
-                message = "Time must be in the format HH:MM AM/PM"
-        )
-        String startTime,
+        @NotBlank(message = "Start time is required")
+        private String startTime;
 
-        @NotBlank(message = "Time is required")
-        @Pattern(
-                regexp = "(?:[01]\\d|2[0-3]):[0-5]\\d (?:AM|PM)",
-                message = "Time must be in the format HH:MM AM/PM"
-        )
-        String endTime,
+        @NotBlank(message = "End time is required")
+        private String endTime;
 
-        @Size(max = 50, message = "Location must not exceed 100 characters")
-        String location,
+        @NotBlank(message = "Location is required")
+        private String location;
 
-
-           @NotBlank(message = "Level is required")
-        @Pattern(
-                regexp = "team|company|department",
-                message = "Level must be 'team','company', 'department'"
-        )
-                String level
-
-
-
-
-) {
+        @NotBlank(message = "Level is required")
+        @Pattern(regexp = "team|company|department", message = "Level must be 'team', 'company', or 'department'")
+        private String level;
 }
+
+
